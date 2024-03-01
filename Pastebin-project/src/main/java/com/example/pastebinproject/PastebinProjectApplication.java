93afcd8fc18bdacd7f46cc44fa20c7654f0c4709
Alibek Birlikbai <alibekbirlikbai21@gmail.com>
2024-03-01 16:37:45 +0600

    project initial configuration

---
+++ Pastebin-project/src/main/java/com/example/pastebinproject/PastebinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pastebinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinProjectApplication.class, args);
+    }
+
+}
---
+++ Pastebin-project/src/test/java/com/example/pastebinproject/PastebinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastebinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
